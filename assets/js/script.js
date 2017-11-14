3fcf2d2a0c89bf55c53615c1b1f58d5663ac1836
Brandon Chow <brandonlchow@berkeley.du>
2017-11-14 00:19:05 -0800

    Hover functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
+	$(".mid-button").hover(function() {
+		if ($(this).is('#aco')) {
+			$("#preview-container").css("background-image", "url('assets/img/aco_1_crop.jpg')");
+		} else if ($(this).is('#maru')) {
+			$("#preview-container").css("background-image", "url('assets/img/maru_1_crop.jpg')");
+		} else if ($(this).is('#kotetsu')) {
+			$("#preview-container").css("background-image", "url('assets/img/kotetsu_1_crop.jpg')");
+		} else if ($(this).is('#gaku')) {
+			$("#preview-container").css("background-image", "url('assets/img/gaku_1_crop.jpg')");
+		}
+	})
+
+	// , function() {
+	// 	$("#preview-container").css("background-image", "url('assets/img/aco_1_crop.jpg')")
+	// }
 
 });
