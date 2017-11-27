ceb5f27a908e49475cd37f089bd5ca18ed17b14c
Brandon Chow <brandonlchow@berkeley.du>
2017-11-26 18:40:47 -0800

    Description page started

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 
 /*--------------MAIN BUTTONS--------------*/
+	
+	
 
 	$(".mid-button").hover(function() {
 		if ($(this).is('#aco')) {
@@ -17,19 +19,30 @@ $(document).ready(function() {
 	transition_in = () => {
 		$("#index").fadeOut();
 		$("#description").fadeIn();
-		// $("#preview").css("animation", "slide 0.8s ease-in 0.2s forwards");
 		$("#preview").addClass("desc");
 	}
 
 	transition_out = () => {
-		$("#preview").removeClass("desc");
-			// $("#preview").css("animation", "reverse-slide 0.8s ease-in 0.2s forwards");
-		$("#index").fadeIn();
 		$("#description").fadeOut();
+		$("#preview").removeClass("desc");
+		$("#index").fadeIn();
 	}
 
 	$(".mid-button").click(function() {
 		transition_in();
+		
+		// setTimeout(function(){
+		// 	if ($(this).is('#aco')) {
+		// 		$("#preview-container").css("background-image", "url('assets/img/aco_1_crop.jpg')");
+		// 	} else if ($(this).is('#maru')) {
+		// 		$("#preview-container").css("background-image", "url('assets/img/maru_1_crop.jpg')");
+		// 	} else if ($(this).is('#kotetsu')) {
+		// 		$("#preview-container").css("background-image", "url('assets/img/kotetsu_1_crop.jpg')");
+		// 	} else if ($(this).is('#gaku')) {
+		// 		$("#preview-container").css("background-image", "url('assets/img/gaku_1_crop.jpg')");
+		// 	}
+		// }, 2000)
+			
 	});
 
 	$("#preview").click(function() {
