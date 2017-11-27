40880585e4d18d1b96816578f1aa21216de257c0
Brandon Chow <brandonlchow@berkeley.du>
2017-11-26 20:48:50 -0800

    Only missing functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,6 +17,7 @@ $(document).ready(function() {
 	});
 
 	transition_in = () => {
+		$(".mid-button").off("hover");
 		$("#index").fadeOut();
 		$("#description").fadeIn();
 		$("#preview").addClass("desc");
@@ -28,20 +29,63 @@ $(document).ready(function() {
 		$("#index").fadeIn();
 	}
 
+	aco = () => {
+		$(".pfp").css("background-image", "url('assets/img/aco_pfp.jpg')");
+		$(".first").css("background-image", "url('assets/img/aco_2.jpg')");
+		$(".second").css("background-image", "url('assets/img/aco_3.jpg')");
+		$(".third").css("background-image", "url('assets/img/aco_4.jpg')");
+		$(".profile-name").text("Aco");
+		$(".profile-handle").text("@acoshiba");
+		$(".quote").text('"Where did the treat go?"');
+	}
+
+	maru = () => {
+		$(".pfp").css("background-image", "url('assets/img/maru_pfp.jpg')");
+		$(".first").css("background-image", "url('assets/img/maru_2.jpg')");
+		$(".second").css("background-image", "url('assets/img/maru_3.jpg')");
+		$(".third").css("background-image", "url('assets/img/maru_4.jpg')");
+		$(".profile-name").text("Maru");
+		$(".profile-handle").text("@marutaro");
+		$(".quote").text('"The smiliest dog in Japan"');
+	}
+
+
+
+	kotetsu = () => {
+		$(".pfp").css("background-image", "url('assets/img/kotetsu_pfp.jpg')");
+		$(".first").css("background-image", "url('assets/img/kotetsu_2.jpg')");
+		$(".second").css("background-image", "url('assets/img/kotetsu_3.jpg')");
+		$(".third").css("background-image", "url('assets/img/kotetsu_4.jpg')");
+		$(".profile-name").text("Kotetsu");
+		$(".profile-handle").text("@ganchi_with_kotetsu");
+		$(".quote").text('"It\'s so ugly that it\'s cute!"');
+	}
+
+	gaku = () => {
+		$(".pfp").css("background-image", "url('assets/img/gaku_pfp.jpg')");
+		$(".first").css("background-image", "url('assets/img/gaku_2.jpg')");
+		$(".second").css("background-image", "url('assets/img/gaku_3.jpg')");
+		$(".third").css("background-image", "url('assets/img/gaku_4.jpg')");
+		$(".profile-name").text("Gaku");
+		$(".profile-handle").text("@shibainu.gaku");
+		$(".quote").text('"This is the ultimate brotherly love"');
+	}
+
 	$(".mid-button").click(function() {
 		transition_in();
-		
-		// setTimeout(function(){
-		// 	if ($(this).is('#aco')) {
-		// 		$("#preview-container").css("background-image", "url('assets/img/aco_1_crop.jpg')");
-		// 	} else if ($(this).is('#maru')) {
-		// 		$("#preview-container").css("background-image", "url('assets/img/maru_1_crop.jpg')");
-		// 	} else if ($(this).is('#kotetsu')) {
-		// 		$("#preview-container").css("background-image", "url('assets/img/kotetsu_1_crop.jpg')");
-		// 	} else if ($(this).is('#gaku')) {
-		// 		$("#preview-container").css("background-image", "url('assets/img/gaku_1_crop.jpg')");
-		// 	}
-		// }, 2000)
+		if ($(this).is('#aco')) {
+			aco();
+			$("#description").addClass("aco");
+		} else if ($(this).is('#maru')) {
+			maru();
+			$("#description").addClass("maru");
+		} else if ($(this).is('#kotetsu')) {
+			kotetsu();
+			$("#description").addClass("kotetsu");
+		} else if ($(this).is('#gaku')) {
+			gaku();
+			$("#description").addClass("gaku");
+		}
 			
 	});
 
